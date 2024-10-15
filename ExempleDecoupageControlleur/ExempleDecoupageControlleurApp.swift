62328a925a43053b35e92a3e8729760773da5344
Enzoait <pcenzox25@gmail.com>
2024-10-15 15:01:14 +0200

    Initial Commit

---
+++ ExempleDecoupageControlleur/ExempleDecoupageControlleurApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExempleDecoupageControlleurApp.swift
+//  ExempleDecoupageControlleur
+//
+//  Created by AIT-YAKOUB Enzo on 15/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct ExempleDecoupageControlleurApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
